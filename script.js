7bab968da91118d96c1ceadb0b96a6f76e57a1d8
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-11-06 15:18:46 +1100

    Added sidebar elements and started sidebar functionality

--- script.js
+++ script.js
@@ -37,21 +37,18 @@ function libraryIntoList(book){
 
 const hobbit = new Book('The Hobbit', 'Tolkien', '250', 'yes');
 const theRing = new Book('The Ring', 'R.R Stein', '500', 'no');
-// addBookToLibrary(hobbit);
-// addBookToLibrary(theRing);
+addBookToLibrary(hobbit);
+addBookToLibrary(theRing);
 // myLibrary.forEach(libraryIntoList);
 
-const addBook = document.querySelector(".addBookBtn");
+
+// const bookDataForm = document.createElement("div");
+// bookDataForm.classList.add("bookInputForm");
+const addBook = document.querySelector(".addNewBookBtn");
 const mainDisplay = document.querySelector(".mainContent");
-
-
-const bookDataForm = document.createElement("div");
-bookDataForm.classList.add("bookInputForm");
-
-
-
 const bookForm = document.querySelector(".addBookForm");
 const bookFormStyle = window.getComputedStyle(bookForm);
+
 addBook.addEventListener("click", function(e){
     if (bookFormStyle.getPropertyValue('visibility') == "visible"){
         bookForm.style.visibility = "hidden";
@@ -68,10 +65,56 @@ const inputBookPages = document.querySelector("#pages");
 
 let newBookInput = "";
 
-const createBook = document.querySelector(".createBookBtn");
+const createBook = document.querySelector(".addBookBtn");
+const readStatusYes = document.querySelector("#readStatusYes");
+const readStatusNo = document.querySelector("#readStatusNo");
+
 createBook.addEventListener("click",function(e){
-    let newBookData = new Book(inputBookTitle.value, inputBookAuthor.value, inputBookPages.value, "no");
+    let readStatusYesNo = "";
+    if ( readStatusYes.checked){
+        readStatusYesNo = "yes";
+    }
+    else if (readStatusNo.checked){
+        readStatusYesNo = "no";
+    }
+    let newBookData = new Book(inputBookTitle.value, inputBookAuthor.value, inputBookPages.value, readStatusYesNo);
     addBookToLibrary(newBookData);
+    if (sideBar.childElementCount <= 5) {
+        addBookToSidebar();
+    }
 });
+
+// SIDEBAR SCRIPTS
+
+const sideBar = document.querySelector(".sideBar");
+
+function fillSideBar(){
+    for(let i = 0; i < Math.min(myLibrary.length, 5); ++i){
+        const sideBarBook = document.createElement("button");
+        let bookName = myLibrary[i].Title;
+        bookName = bookName.replace(/\s+/g, '');
+        sideBarBook.classList.add("sideBarBook");
+        sideBarBook.classList.add(bookName);
+        sideBarBook.textContent = myLibrary[i].Title;
+        sideBar.appendChild(sideBarBook);
+    }
+};
+function addBookToSidebar(){
+    let bookNumber = myLibrary.length -1;    
+    let bookName = myLibrary[bookNumber].Title;
+    bookName = bookName.replace(/\s+/g, '');
+    const sideBarBook = document.createElement("button");
+    sideBarBook.classList.add("sideBarBook");
+    sideBarBook.classList.add(bookName);
+    sideBarBook.textContent = myLibrary[bookNumber].Title;
+    sideBar.appendChild(sideBarBook);
+
+}
+
+fillSideBar();
+
+
+
+
 
 
